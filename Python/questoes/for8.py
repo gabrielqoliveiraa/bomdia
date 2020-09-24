9f95cb075ebc96a554c4e24dc71ad38caa50aa39
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-24 10:13:51 -0300

    Update for8.py

--- Python/questoes/for8.py
+++ Python/questoes/for8.py
@@ -1,14 +1,17 @@
 idadebase = 0
+idade2 = 0
 hMax = ''
 hiiMax = 0 
+countMulher = 0
 
 for c in range(1, 4):
     idadebase += 1
     nome = input('Digite seu nome: ')
     idade = int(input('Digite sua idade: '))
     sexo = input('Digite seu Sexo: M OU F ')
+    idade2 = idade2 + idade
     if idadebase == 3:
-       idade/3
+       idade2 = idade2/3
     if c == 1:
         hMax = nome
         idade = hiiMax
@@ -16,9 +19,15 @@ for c in range(1, 4):
         if idade > hiiMax:
             nome = hMax
             idade = hiiMax
+    if idade < 18 and sexo == 'F':
+        countMulher += 1
 
-print(idade)
+
+
+
+print('{:.2f}'.format(idade2))
 print(hMax)
+print(countMulher)
     
 
         
