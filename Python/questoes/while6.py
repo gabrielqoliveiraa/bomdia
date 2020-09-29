36c9112a5373e8ffcc9956b1d80ba31d6a3d8612
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-29 10:35:23 -0300

    PA

---
+++ Python/questoes/while6.py
@@ -0,0 +1,3 @@
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razao: '))
+count = 0
---
+++ Python/while5.py
@@ -0,0 +1,12 @@
+razao = int(input('Digite a razão da PA: '))
+ftermo = int(input('Digite o primeiro termo: '))
+count = 0 
+n = 1
+
+while count <= 9:
+    count += 1
+    termo = ftermo + ((n-1)*razao)
+    n += 1
+    print(termo, end=' -> ')
+    
+print('FIM')
