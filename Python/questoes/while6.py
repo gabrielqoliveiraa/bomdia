f99c584f9692c34c96460c0cf6c8d2b064f24b1a
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-29 10:51:01 -0300

    Update while6.py

--- Python/questoes/while6.py
+++ Python/questoes/while6.py
@@ -1,3 +1,23 @@
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razao: '))
-count = 0
+count = 1
+termo = primeiro
+
+while count <= 11:
+    termo = termo + razao
+    count += 1
+    print(termo, end= ' ')
+    if count == 10:
+        escolha = int(input('Você deseja continuar ? \n 1 - Sim \n 2 - Não'))
+        if escolha == 1:
+            escolha1 = int(input('Até qual termo ? '))
+            while count <= escolha1:
+                termo = termo + razao
+                print('Sua nova PA É:')
+                print(termo, end=(' '))
+            
+            
+
+
+
+
