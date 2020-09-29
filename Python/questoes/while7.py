5c397d98fb05e67da09858c9fd5b4eb54e09745d
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-29 12:00:42 -0300

    Update while7.py

--- Python/questoes/while7.py
+++ Python/questoes/while7.py
@@ -1,1 +1,19 @@
-a
+print('=*'*20)
+print('SEQUÊNCIA DE FIBONACCI')
+print('=*'*20)
+a = int(input('Digite o número de elementos da sequencia que você quer ver: '))
+count = 3
+termo1 = 0
+termo2 = 1
+print(termo1, termo2, end=' -> ')
+
+while count <= a:
+    termo3 = termo1 + termo2
+    print(termo3, end=' ')
+    termo1 = termo2
+    termo2 = termo3
+
+print('FIM')
+    
+
+     
