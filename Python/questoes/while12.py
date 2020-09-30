dc252876bd587a798b138cb95e3a3f53707c8ed5
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-29 23:10:12 -0300

    Update while12.py

--- Python/questoes/while12.py
+++ Python/questoes/while12.py
@@ -1,10 +1,11 @@
 from random import randint
-n = soma = 0 
+n = soma = count = 0
 lista = randint(1, 2)
 while True:
     n = int(input('Digite um numero: '))
     escolha = str(input('Par ou Impar ? I/P: ')).upper().strip()[0]
     soma = n + lista
+    count += 1
     if escolha == 'P' and soma % 2 == 0:
         print(f'O computador jogou {lista} A Soma deu {soma} deu Par')
         print('Voce ganhou')
@@ -15,7 +16,7 @@ while True:
         print(f'O computador jogou {lista} A Soma deu {soma}')
         break
 
-print ('Acabou o jogo !! Você perdeu')
+print ('Acabou o jogo !! Você ganhou {} vezes'.format(count))
 
   
     
