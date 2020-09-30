deb2d198ddb20bebfe3652b6670e1306f252e8e7
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-09-29 22:38:13 -0300

    Update while11.py

--- Python/questoes/while11.py
+++ Python/questoes/while11.py
@@ -1,7 +1,7 @@
 count = n = 0
 while True:
     n = int(input('Digite um número: '))
-    while n < 10:
+    while count < 10:
         count += 1
         multi = n*count
         print (f'{n} x {count} = {multi}')
