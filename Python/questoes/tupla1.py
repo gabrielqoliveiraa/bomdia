117eac6f6bedb43f14a4b563555e050866b17e37
Gabriel <gabrielqoliveiraa@gmail.com>
2020-10-08 17:12:04 -0300

    Tuplas

--- Python/questoes/tupla1.py
+++ Python/questoes/tupla1.py
@@ -1,3 +1,26 @@
-lanche = ('Hamburger', 'Suco', 'Pudim')
+numero = ('Zero','Um', 'Dois', 'Tres', 'Quatro', 'Cinco','Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quartorze', 'Quinze','Dezesseis', 'Dezessete','Dezoito', 'Dezenove', 'Vinte')
 
-print(lanche[1])
+while True:
+    resp = int(input('Digite um número entre 0 e 20: '))
+    while resp > 20 or resp < 0:
+        resp = int(input('Tente Novamente. Digite um número de 0 a 20: '))
+    print (numero[resp])
+    usuario = str(input('Deseja continuar S/N ? ')).upper().strip()[0]
+
+    if usuario == 'N':
+        break
+
+
+
+
+    
+
+
+print("Obrigado")
+
+    
+
+
+   
+
+
