43f59c2495b991890f4fca4faec4590ef339a6f8
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-10-19 20:41:13 -0300

    Create lista9.py

---
+++ Python/questoes/lista9.py
@@ -0,0 +1,10 @@
+lista = []
+temp = []
+
+while True:
+    temp.append(str(input('Nome: ')))
+    nota1 = (float(input('Nota 1: ')))
+    nota2 = (float(input('Nota 2: ')))
+    media = (nota1+nota2)/2
+    
+    
