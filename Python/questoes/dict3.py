f5da2d8f5ee5b111d6770e69c3bae3d20db52109
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-10-21 01:12:22 -0300

    Update dict3.py

--- Python/questoes/dict3.py
+++ Python/questoes/dict3.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 cadastro = {}
 cadastro['nome'] = str(input('Nome: '))
-cadastro['idade'] = (2020 - int(input('Ano de nascimento: ')))
+cadastro['idade'] = (datetime.now().year - int(input('Ano de nascimento: ')))
 cadastro['ctps'] = int(input('Carteira de trabalho (0 não tem) '))
 if cadastro['ctps'] != 0:
     cadastro['contratação'] = int(input('Ano de contratação: '))
     cadastro['salario'] = float(input('Salário: R$ '))
-    cadastro['aposentadoria'] = ((cadastro['contratação'] - cadastro['idade']) + 35)
+    cadastro['aposentadoria'] = (cadastro['contratação']  + 35)
 print('-='*30)
 for k,v in cadastro.items():
     print(f'{k} tem valor {v}')
