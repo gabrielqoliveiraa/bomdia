56c102fc9c8ef7bd7f67fe71d58c5cc9198f353c
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-10-24 22:17:00 -0300

    Update funct4.py

--- Python/func/funct4.py
+++ Python/func/funct4.py
@@ -1,15 +1,18 @@
 from time import sleep
 
 def contador(i, f, p):
-    print('A)PRIMEIRA CONTAGEM - 1 ATÉ 10 / 1-1')
     if i < f:
         cont = i
         while cont <= f:
-            print(f'cont', end=' ')
-            sleep(0.5)
-            cont += p 
-
-
+            print(f'{cont}', end=' ')
+            sleep(1)
+            cont += p
+    
 contador(1, 10, 1)
+
+    
+
+
+
 
 
