f4bbb1968846e26e532e81ef1d814628ca92c35f
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-10-24 22:25:09 -0300

    Update funct4.py

--- Python/func/funct4.py
+++ Python/func/funct4.py
@@ -1,14 +1,33 @@
 from time import sleep
 
 def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+
+
     if i < f:
         cont = i
         while cont <= f:
             print(f'{cont}', end=' ')
-            sleep(1)
+            #sleep(0.4)
             cont += p
-    
+        print('FIM')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont}', end=' ') 
+            #sleep(0.4)
+            cont -= p
+        print('FIM')
+
 contador(1, 10, 1)
+contador(10, 0, 2)
+inicioUsuario = int(input('INICIO: '))
+fimUsuario = int(input('FIM: '))
+passoUsuario = int(input('PASSO: '))
+contador(inicioUsuario, fimUsuario, passoUsuario)
 
     
 
