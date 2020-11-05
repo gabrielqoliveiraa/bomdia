874fb96680f617ebd61f626d91738ef4c861b16a
gabrielqoliveiraa <56687190+gabrielqoliveiraa@users.noreply.github.com>
2020-11-04 23:33:09 -0300

    Teste Js

--- Python/func/function11.py
+++ Python/func/function11.py
@@ -1,23 +1,11 @@
-def notas(*num):
-    num = {}
-    maior = menor = 0
-    count = 0
-    for c in num:
-        if count == 0:
-            maior = num[0]
-            menor = num[0]
-        else:
-            if num[c] > maior:
-                maior = num[c]
-            elif num[c] < menor:
-                menor = num[c]
-        count += 1
-    soma = sum(num)
-    num['media'] = soma/len(num)
-    num['maior'] = maior
-    num['menor'] = menor 
-    num['total'] = len(num)
-    return num
+def notas(*num, sit=False):
+    r = dict()
+    r['total'] = len(num)
+    r['maior'] = max(num)
+    r['menor'] = min(num)
+    r['média'] = sum(num)/len(num)
+
+    return r
 
 
 
